c5cb74f09827f33e08ae10367573f56bc794b0cc
Mehradi <mehradi.info@gmail.com>
2023-07-16 08:44:46 +0200

    Generating Database via prisma

---
+++ prisma/products.ts
@@ -0,0 +1,27 @@
+const products = [
+  {
+    name: 'Nostrud adipisicing dolor qui excepteur.',
+    price: 56.6,
+    image: 'https://picsum.photos/200',
+    description:
+      'Consectetur sunt eiusmod fugiat tempor. Id tempor consectetur quis nostrud sint id ea enim deserunt. Reprehenderit voluptate duis id culpa dolore velit incididunt fugiat do exercitation ut. Fugiat cupidatat quis ullamco Lorem magna dolor veniam ad exercitation commodo. Exercitation pariatur cupidatat dolore eiusmod.',
+    brand: 'Alibaba',
+  },
+  {
+    name: 'Excepteur nostrud ex consequat duis.',
+    price: 70.3,
+    image: 'https://picsum.photos/200',
+    description:
+      'Consectetur sunt eiusmod fugiat tempor. Id tempor consectetur quis nostrud sint id ea enim deserunt. Reprehenderit voluptate duis id culpa dolore velit incididunt fugiat do exercitation ut. Fugiat cupidatat quis ullamco Lorem magna dolor veniam ad exercitation commodo. Exercitation pariatur cupidatat dolore eiusmod.',
+    brand: 'Youtube',
+  },
+  {
+    name: 'Labore incididunt amet enim laboris laborum nostrud culpa anim mollit.',
+    price: 35.2,
+    image: 'https://picsum.photos/200',
+    description:
+      'Consectetur sunt eiusmod fugiat tempor. Id tempor consectetur quis nostrud sint id ea enim deserunt. Reprehenderit voluptate duis id culpa dolore velit incididunt fugiat do exercitation ut. Fugiat cupidatat quis ullamco Lorem magna dolor veniam ad exercitation commodo. Exercitation pariatur cupidatat dolore eiusmod.',
+    brand: 'Github',
+  },
+];
+export default products;
---
+++ prisma/seed.ts
@@ -0,0 +1,18 @@
+import { PrismaService } from 'src/prisma/prisma.service';
+import products from './products';
+
+const prisma = new PrismaService();
+
+(function seed() {
+  products.forEach(async (p) => {
+    await prisma.product.create({
+      data: {
+        name: p.name,
+        description: p.description,
+        price: p.price,
+        image: p.image,
+        brand: p.brand,
+      },
+    });
+  });
+})();
